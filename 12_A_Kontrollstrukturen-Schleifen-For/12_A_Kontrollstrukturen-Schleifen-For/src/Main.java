97ff16c3483baff34a728145e265da97e00fc2c7
Tadlyy <tadejspionjak09@gmail.com>
2025-11-12 11:27:32 +0100

    Aufgabe 2

--- 12_A_Kontrollstrukturen-Schleifen-For/12_A_Kontrollstrukturen-Schleifen-For/src/Main.java
+++ 12_A_Kontrollstrukturen-Schleifen-For/12_A_Kontrollstrukturen-Schleifen-For/src/Main.java
@@ -24,7 +24,7 @@ public class Main {
         //      it would repeat the same thing all the time
         //      because there is no iterator++
         //      put an iretator++
-        
+
         //--------------------------------------------------------------------------------------------------------------
         System.out.println("Exercise 02");
         // 02.  Using for loops, print each task and its numbers to the console (all numbers inclusive):
@@ -39,6 +39,79 @@ public class Main {
         //      02-09. Even numbers from 10 to -10 (descending)
         //      02-10. Odd numbers from 10 to -10 (descending)
 
+
+        System.out.println("02-01");
+        for (int num1 = 0; num1 <= 100;){
+            System.out.println("Nummer "+num1);
+            num1++;
+        }
+
+
+        System.out.println("02-02");
+        for (int num2 = 42; num2 <= 100;){
+            System.out.println("Nummer "+num2);
+            num2++;
+        }
+
+
+        System.out.println("02-03");
+        for (int num3 = 42; num3 <= 123;){
+            System.out.println("Nummer "+num3);
+            num3++;
+        }
+
+
+        System.out.println("02-04");
+        for (int num4 = 0; num4 <= 10;){
+            System.out.println("Nummer "+num4);
+            num4++;
+            num4++;
+        }
+
+
+        System.out.println("02-05");
+        for (int num5 = 1; num5 <= 10;){
+            System.out.println("Nummer "+num5);
+            num5++;
+            num5++;
+        }
+
+
+        System.out.println("02-06");
+        for (int num6 = -10; num6 <= 20;){
+            System.out.println("Nummer "+num6);
+            num6++;
+        }
+
+
+        System.out.println("02-07");
+        for (int num7 = 35; num7 >= 0;){
+            System.out.println("Nummer "+num7);
+            num7--;
+        }
+
+
+        System.out.println("02-08");
+        for (int num8 = 15; num8 >= -20;){
+            System.out.println("Nummer "+num8);
+            num8--;
+        }
+
+
+        System.out.println("02-09");
+        for (int num9 = 10; num9 >= -10;){
+            System.out.println("Nummer "+num9);
+            num9--;
+            num9--;
+        }
+
+
+        System.out.println("02-10");
+        for (int num10 = 9; num10 >= -10;){
+            System.out.println("Nummer "+num10);
+            num10--;
+            num10--;
+        }
 
         //--------------------------------------------------------------------------------------------------------------
         System.out.println("Exercise 03");
