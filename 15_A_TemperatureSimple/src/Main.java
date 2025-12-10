950e19e089d1a73b67d872c0d33679cb5e183470
Tadlyy <tadejspionjak09@gmail.com>
2025-12-10 11:35:51 +0100

    Aufgabe 17

--- 15_A_TemperatureSimple/src/Main.java
+++ 15_A_TemperatureSimple/src/Main.java
@@ -1,8 +1,12 @@
 import  java.util.Scanner;
+import com.berufsfachschule.temperature.Temperature;
 
 public class Main {
     public static void main(String[] args) {
+        Temperature temp1 = new Temperature();
+        temp1.celsius = -5.37;
 
+        temp1.printCelsius();
 
     }
 }
---
+++ 15_A_TemperatureSimple/src/com/berufsfachschule/temperature/Temperature.java
@@ -0,0 +1,14 @@
+package com.berufsfachschule.temperature;
+
+public class Temperature {
+
+    public double celsius;
+
+    public void printCelsius (){
+        System.out.println(this.celsius + "°C");
+    }
+
+
+
+
+}
