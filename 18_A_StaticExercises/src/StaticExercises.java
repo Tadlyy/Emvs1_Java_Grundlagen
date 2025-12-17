c32d639ad0c91a1f5c0b817997cbd898d5238a8c
Tadlyy <tadejspionjak09@gmail.com>
2025-12-17 09:28:14 +0100

    Aufgaben 18

--- 18_A_StaticExercises/src/StaticExercises.java
+++ 18_A_StaticExercises/src/StaticExercises.java
@@ -1,2 +1,28 @@
 public class StaticExercises {
+
+    public int testInteger = 5;
+
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+
+        //Math.PI=PI
+        //Math.E=E
+        //Math.round =round number
+        //Math.cos=Returns the trigonometric tangent of an angle.
+
+        Utils.print("Hello World!");
+        Utils.print(2.35f);
+        Utils.print(123.8175419823d);
+        Utils.print(1238175419823L);
+        Utils.print(true);
+        Utils.print(false);
+        Utils.print('a');
+        Utils.print((byte) 32);
+        Utils.print(-5000);
+
+
+
+
+
+    }
 }
---
+++ 18_A_StaticExercises/src/Utils.java
@@ -0,0 +1,38 @@
+public class Utils {
+
+    public static void print(String s) {
+        System.out.println(s);
+    }
+
+
+    public static void print(byte b) {
+        System.out.println(b);
+    }
+
+    public static void print(int i) {
+        System.out.println(i);
+    }
+
+    public static void print(long l) {
+        System.out.println(l);
+    }
+
+    public static void print(float f) {
+        System.out.println(f);
+    }
+
+    public static void print(double d) {
+        System.out.println(d);
+    }
+
+    public static void print(char c) {
+        System.out.println(c);
+    }
+
+    public static void print(boolean b) {
+        System.out.println(b);
+    }
+
+}
+
+
